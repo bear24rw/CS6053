c352a825e0a57c3aa06f04df44dece47c3921e1c
Max Thrun <bear24rw@gmail.com>
2014-02-11 12:22:05 -0500

    added encryption support to client and server

--- client.py
+++ client.py
@@ -1,8 +1,17 @@
 import socket
 from printer import Printer
 from config import Config
+from diffie_hellman import DHE
+from karn import Karn
+
+printer = Printer("client")
+dhe = DHE()
+authenticated = False
+karn = None
 
 def generate_response(line):
+    global karn
+    global authenticated
 
     line = line.strip()
     directive, args = [x.strip() for x in line.split(':', 1)]
@@ -11,51 +20,75 @@ def generate_response(line):
 
     if directive == "REQUIRE":
         if args == "IDENT":
-            return "IDENT %s\n" % Config.ident
+            if Config.use_encryption:
+                return "IDENT %s %s" % (Config.ident, dhe.public_key)
+            else:
+                return "IDENT %s" % Config.ident
         elif args == "PASSWORD":
-            return "PASSWORD %s\n" % Config.password
+            return "PASSWORD %s" % Config.password
         elif args == "ALIVE":
             if Config.cookie == "":
                 printer.info("Alive request but we don't know the cookie!")
             else:
-                return "ALIVE %s\n" % Config.cookie
+                return "ALIVE %s" % Config.cookie
         elif args == "HOST_PORT":
-            return "HOST_PORT %s %s\n" % (Config.host_ip, Config.host_port)
+            return "HOST_PORT %s %s" % (Config.host_ip, Config.host_port)
         else:
             printer.error("Unknown require: " + line)
     elif directive == "RESULT":
         args = args.split(' ', 1)
-        if args[0] == "PASSWORD":
+        if args[0] == "IDENT":
+            dhe.monitor_key(args[1])
+            karn = Karn(dhe.secret)
+            printer.info("Setup secret key")
+        elif args[0] == "PASSWORD":
             Config.cookie = args[1]
             printer.info("Got cookie: " + Config.cookie)
         elif args[0] == "HOST_PORT":
             printer.info("Login successful! (%s)" % args[1])
-        if args[0] == "ALIVE" and args[1] == "Identity has been verified.":
+        elif args[0] == "ALIVE" and args[1] == "Identity has been verified.":
             printer.info("Alive verified")
+            authenticated = True
         else:
-            printer.error("Unknown result: " + line)
+            printer.error("Unknown result: %s (args: %r)" % (line, args))
     elif directive == "WAITING":
-        pass
+        if authenticated:
+            return raw_input('Command: ')
     elif directive == "COMMENT":
         pass
     elif directive == "COMMAND_ERROR":
         if "unable to connect to host" in args:
-            return ""
+            return None
         else:
             printer.error(line)
     else:
         printer.error("Unknown directive: " + line)
 
-    return ""
+    return None
 
 if __name__ == "__main__":
-
-    printer = Printer("client")
 
     sock = socket.create_connection((Config.monitor_ip, Config.monitor_port))
 
     for line in sock.makefile():
+
+        # check if this line is encrypted
+        if line.startswith('1a'):
+            line = karn.decrypt(line)
+            if line is None: continue
+
+        # generate the response for this line
         response = generate_response(line)
+
+        # if there is no response go get another line
+        if response is None: continue
+
+        # if we have a valid karn we can encrypt the response
+        if karn:
+            response = karn.encrypt(response)
+
+        # add the newline and send the response
+        response += '\n'
         printer.command(response)
         sock.send(response)
 
--- server.py
+++ server.py
@@ -1,13 +1,22 @@
 import SocketServer
 from config import Config
 from printer import Printer
+from diffie_hellman import DHE
+from karn import Karn
 
 class tcp_handler(SocketServer.StreamRequestHandler):
 
     def setup(self):
 
+        self.dhe = DHE()
+        self.karn = None
+
         self.printer = Printer("server")
         SocketServer.StreamRequestHandler.setup(self)
+
+    def finish(self):
+
+        SocketServer.StreamRequestHandler.finish(self)
 
     def generate_response(self, line):
 
@@ -18,19 +27,26 @@ class tcp_handler(SocketServer.StreamRequestHandler):
 
         if directive == "REQUIRE":
             if args == "IDENT":
-                return "IDENT %s\n" % Config.ident
+                if Config.use_encryption:
+                    return "IDENT %s %s" % (Config.ident, self.dhe.public_key)
+                else:
+                    return "IDENT %s" % Config.ident
             elif args == "ALIVE":
                 if Config.cookie == "":
                     self.printer.info("Alive request but we don't know the cookie!")
                 else:
-                    return "ALIVE %s\n" % Config.cookie
+                    return "ALIVE %s" % Config.cookie
             elif args == "QUIT":
-                return "QUIT\n"
+                return "QUIT"
             else:
                 self.printer.error("Unknown require: " + line)
         elif directive == "RESULT":
             args = args.split(' ', 1)
-            if args[0] == "ALIVE" and args[1] == "Identity has been verified.":
+            if args[0] == "IDENT":
+                self.dhe.monitor_key(args[1])
+                self.karn = Karn(self.dhe.secret)
+                self.printer.info("Setup secret key")
+            elif args[0] == "ALIVE" and args[1] == "Identity has been verified.":
                 self.printer.info("Alive verified")
             elif args[0] == "QUIT":
                 self.printer.info("server has quit")
@@ -45,12 +61,29 @@ class tcp_handler(SocketServer.StreamRequestHandler):
         else:
             self.printer.error("Unknown directive: " + line)
 
-        return ""
+        return None
 
     def handle(self):
 
         for line in self.rfile:
+
+            # check if this line is encrypted
+            if line.startswith('1a'):
+                line = self.karn.decrypt(line)
+                if line is None: continue
+
+            # generate the response for this line
             response = self.generate_response(line)
+
+            # if there is no response go get another line
+            if response is None: continue
+
+            # if we have a valid karn we can encrypt the response
+            if self.karn:
+                response = self.karn.encrypt(response)
+
+            # add the newline and send the response
+            response += '\n'
             self.printer.command(response)
             self.wfile.write(response)
 

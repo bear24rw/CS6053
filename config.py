276aab5bc053e8b4b3b58b758d03f68a7714f17c
Max Thrun <bear24rw@gmail.com>
2014-02-11 12:21:41 -0500

    update ident and cookie in config. add 'use_encryption' flag

--- config.py
+++ config.py
@@ -3,8 +3,9 @@ import socket
 class Config:
     monitor_ip = "helios.ececs.uc.edu"
     monitor_port = 8180
-    ident = "mtest14"
+    ident = "mtest16"
     password = "12345"
-    cookie = "QG1CJAW61KEU9FZYPFH"
+    cookie = "C5K8GNM22KQ5XCFKVHF"
     host_ip = socket.getfqdn()
-    host_port = 5000
+    host_port = 35201
+    use_encryption = True
